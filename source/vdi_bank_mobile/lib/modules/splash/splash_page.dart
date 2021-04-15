5351ca8385d02b8a78bf2d239a956136a1433e62
kha <hoangkha.tp@gmail.com>
2021-04-15 20:50:09 +0700

    splash

--- source/vdi_bank_mobile/lib/modules/splash/splash_page.dart
+++ source/vdi_bank_mobile/lib/modules/splash/splash_page.dart
@@ -1,22 +1,12 @@
-import 'package:vdi_bank/core/services/auth_service.dart';
-import 'package:vdi_bank/core/services/connectivity_service.dart';
-import 'package:vdi_bank/core/services/custom_dialog_service.dart';
-import 'package:vdi_bank/core/services/localization_service.dart';
-import 'package:vdi_bank/core/utils/util_color.dart';
-import 'package:vdi_bank/routes/pages.dart';
-import 'package:vdi_bank/theme/colors_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
+import 'package:vdi_bank/core/utils/util_color.dart';
+import 'package:vdi_bank/modules/splash/splash_controller.dart';
+import 'package:vdi_bank/theme/colors_theme.dart';
 
-class SplashPage extends StatefulWidget {
-  @override
-  _SplashPageState createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-
+class SplashPage extends GetView<SplashController> {
+  final SplashController c = Get.put(SplashController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
